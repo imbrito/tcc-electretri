9af567b56ff73a96631901bb245b92ed373775cf
imbrito <ismael.20rj@gmail.com>
2015-08-23 14:03:05 -0300

    #4 refactor controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!, :except => [:about, :team]
+  before_filter :get_user
+
+  def get_user
+  	@user = current_user
+  end
   def index
-  	@count = Project.where(:user_id => current_user.id).count
+  	@count = @user.projects.count
   end
 
   def about
