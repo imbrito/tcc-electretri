f21fe8959fa3ab67032033b83972ab8090c8be57
imbrito <ismael.20rj@gmail.com>
2016-07-25 00:30:02 -0300

    fix rubocop issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "codeclimate-test-reporter"
+require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
